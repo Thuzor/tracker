ae556b774189de79f452c2c8a553aec89562a93c
Adam Thewlis <github@inboxy.co.uk>
2019-11-08 10:43:38 +0000

    API - Simplify response

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,7 +1,5 @@
 export default (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 200;
-    res.end(JSON.stringify({
+    res.status(200).json({
         test: "Hello World"
-    }))
+    });
 }
